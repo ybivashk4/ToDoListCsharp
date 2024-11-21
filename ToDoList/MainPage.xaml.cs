aa52e0570936f033fd8c1be4951939a86fc93b6d
mihail B <beltyukov-mihail@mail.ru>
2024-11-21 11:53:21 +0500

    1

--- ToDoList/MainPage.xaml.cs
+++ ToDoList/MainPage.xaml.cs
@@ -1,24 +1,112 @@
-﻿namespace ToDoList
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ToDoList
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MyViewModel();
+        }
+        // None
+        // Strikethrough
+        public class MyViewModel : INotifyPropertyChanged {
+            public ObservableCollection<MyTask> Items { get; set; }
+            public ICommand AddCommand { get; set; }
+            public ICommand AddStrokeCommand { get; set; }
+            public ICommand RemoveCommand { get; set; }
+
+            private string _entryText;
+
+            public string EntryText
+            {
+                get => _entryText;
+                set
+                {
+                    _entryText = value;
+                    OnPropertyChanged(nameof(EntryText));
+                }
+            }
+
+            public MyViewModel() {
+                Items = new ObservableCollection<MyTask>();
+                AddCommand = new Command<object>(addTask);
+                AddStrokeCommand = new Command<object>(ToggleTask);
+                RemoveCommand = new Command<MyTask>(removeFromItems);
+
+            }
+
+            private void addTask(object parametr)
+            {
+                if (parametr is string taskName && !string.IsNullOrWhiteSpace(taskName))
+                {
+                    Items.Add(new MyTask { Name = taskName });
+                    EntryText = string.Empty; 
+                }
+            }
+
+            private void removeFromItems (MyTask parametr)
+            {
+                if (parametr != null)
+                {
+                    Items.Remove(parametr);
+                }
+            }
+
+            private void ToggleTask(object parametr)
+            {
+                if (parametr is MyTask task)
+                {
+                    task.IsDone = !task.IsDone;
+                }
+            }
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        public class MyTask : INotifyPropertyChanged
         {
-            count++;
+            private bool _is_done;
+            private string _name;
+            public string Name
+            {
+                get => _name;
+                set
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+            public bool IsDone
+            {
+                get => _is_done;
+                set
+                {
+                    _is_done = value;
+                    OnPropertyChanged(nameof(IsDone));
+                    OnPropertyChanged(nameof(Stroke));
+                }
+            }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            public TextDecorations Stroke => IsDone ? TextDecorations.Strikethrough : TextDecorations.None;
+            
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 
